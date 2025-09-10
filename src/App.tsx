f8ce7af1c82cd3ed45f09dafab81db6e3f440de3
Moses Rametsi <mosesrametsi16@gmail.com>
2025-09-10 23:07:19 +0200

    Remove colon and extra spaces.

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,15 @@ const OtherComponent = ({ count }: { count: number }) => {
   const increment = useCounterStore((state) => state.increment);
   const decrement = useCounterStore((state) => state.decrement);
 
-  return <div>
-            {count}
-            <div>
-              <button onClick={increment}>Increment :</button>
-              <button onClick={decrement}>: Decrement</button>
-            </div>
-         </div>;
+  return (
+    <div>
+      {count}
+      <div>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
