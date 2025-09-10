883a55b55ac946e3d545340ce166157e2439d000
Moses Rametsi <mosesrametsi16@gmail.com>
2025-09-10 22:12:38 +0200

    Change file name and debug code logic.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+const App = () => {
+  const count = useCounterStore((state) => state.count);
+
+  return <OtherComponent count={count} />;
+};
+
+const OtherComponent = ({ count }: { count: number }) => {
+  return <div>{count}</div>;
+};
+
+export default App;
